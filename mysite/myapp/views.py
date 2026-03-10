b8d16b96ec8d2d98d8c60657bcf59682f1d29930
divyanshi2203 <divyanshisaini22@gmail.com>
2026-03-10 23:12:07 +0530

    tailwind progress

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -3,11 +3,16 @@ from django.http import HttpResponse
 from .models import Item
 
 def home(request):
-    return HttpResponse("This is my new app.")
-
-def details(request):
     item_list = Item.objects.all()
     context = {
         "item_list":item_list    
         }
+    return render(request,"myapp/index.html",context)
+
+
+def details(request,id):
+    item = Item.objects.get(id=id)
+    context = {
+        "item" : item
+    }
     return render(request,"myapp/details.html",context)
